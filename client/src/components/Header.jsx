444cdac48334010f517f6b51496dfbe5909600ae
Rishi <rishirohilla14@gmail.com>
2024-03-07 13:08:22 +0530

    Added folder-name to the repository

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {FaSearch} from 'react-icons/fa'
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-red-400">Riyal</span>
+            <span className="text-red-800">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center w-[30%] justify-between">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-[80%]"
+          />
+          <FaSearch className="text-slate-600"/>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-800 hover:underline hover:text-slate-600">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-800 hover:underline hover:text-slate-600">
+              About
+            </li>
+          </Link>
+          <Link to="sign-in">
+            <li
+              className="hidden sm:inline te
+                xt-slate-800 hover:underline hover:text-slate-600"
+            >
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1>signup
+      </h1>
+    </div>
+  )
+}
