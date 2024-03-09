cd81433f2cad819aadd786ca23166e4f3d7f4d3d
Rishi <rishirohilla14@gmail.com>
2024-03-10 04:22:10 +0530

    created listing api route

--- api/index.js
+++ api/index.js
@@ -4,27 +4,23 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from "cookie-parser";
-
+import listingRouter from './routes/listing.route.js'
 dotenv.config();
 mongoose.connect(process.env.mongo_url).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error(err);
 });
-
 const app= express();
-
 app.use(express.json());
-
 app.use(cookieParser());
-
-
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT} ` );
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
