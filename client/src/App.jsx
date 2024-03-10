9961d4a4c26ba587884e94e2f136c1847a9b19bb
Rishi <rishirohilla14@gmail.com>
2024-03-11 00:47:35 +0530

    completed edit listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 export default function App() {
   return (
     // <p className='text-red-600 font-semibold'>Hello World!</p>
@@ -19,8 +20,9 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/create-listing" element={<CreateListing />}/>
+          <Route path="/update-listing/:listingId" element={<UpdateListing />}/>
         </Route>
       </Routes>
     </BrowserRouter>
