b9c775506dfe90830fd0e6a191f3a8a28d6e2ffc
Rishi <rishirohilla14@gmail.com>
2024-03-10 20:10:10 +0530

    completed cretae listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -177,6 +177,7 @@ export default function Profile() {
         <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading....' : 'Update'}
         </button>
+        <Link className="bg-green-600 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 " to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-600 cursor-pointer">Delete Account</span>
