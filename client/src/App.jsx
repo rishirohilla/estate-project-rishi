c77b96184f21bcae0f416c18d4219d33f4377b59
Rishi <rishirohilla14@gmail.com>
2024-03-11 14:38:19 +0530

    created image slider in listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 export default function App() {
   return (
     // <p className='text-red-600 font-semibold'>Hello World!</p>
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/listing/:listingId" element={<Listing />}></Route>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />}/>
           <Route path="/create-listing" element={<CreateListing />}/>
